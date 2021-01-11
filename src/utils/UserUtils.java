aa86417eb201961533e4a94b88631b2a82ccbb2e
Mārcis Andersons <marcis.andersons0925@gmail.com>
2021-01-12 01:49:21 +0200

    Final commit.

--- src/utils/UserUtils.java
+++
@@ -1,7 +0,0 @@
-package utils;
-
-public class UserUtils {
-    public static final String DATABASE_HOST = "jdbc:mysql://localhost:3306/musicmanagementapplication?useSSL=false&serverTimezone=UTC";
-    public static final String DATABASE_USERNAME = "Marcis Andersons";
-    public static final String DATABASE_PASSWORD = "M@rchel0925";
-}
